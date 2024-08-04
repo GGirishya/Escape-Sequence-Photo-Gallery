4d26a02d60c56956dcdedc9805868ae7c421b3b4
Guillaume Girishya <143455392+GGirishya@users.noreply.github.com>
2024-08-04 07:23:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const imageSources = [
+  "assets/trail.jpg",
+  "assets/lakesunset.jpg",
+  "assets/cabin.jpg",
+  "assets/doe.jpg",
+  "assets/starrynight.jpg",
+];
+
+function addThumbnail(imageSource) {
+  // 1.1 Create the element
+  
+  // 1.2 Customize the element
+  
+  // 1.3 Append the element
+  
+  // 1.4 Add the onclick
+  
+}
+
+function addAllThumbnails() {
+  // 2.1 Loop through imageSources
+  
+  // 2.2 Call addThumbnail each iteration 
+  
+}
+
+// 2.3 Call addAllThumbnails
+
+
+// LevelUp Display the first image on page load
+
+
